965d365adf2b97e8d557b3559b06853f2d22311e
Yhon J. Ochoa <105899541+YhonJ8a@users.noreply.github.com>
2026-05-24 00:14:16 -0500

    chore(config): migrate eslint to v9 flat config

---
+++ apps/api/eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error",
+            "@typescript-eslint/explicit-function-return-type": "warn",
+        },
+    }
+);
